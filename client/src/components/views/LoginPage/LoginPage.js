1810766e45a23a604cb15eba9019b3c0862fedc8
sangmi <jude4264@gmail.com>
2022-08-04 16:27:22 +0900

    [UPDATE] 22강 진행중

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+// import { response } from 'express'
 
 function LoginPage() {
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/hello')
+    .then(response => console.log(response.data))
+
+  }, [])
+
+
   return (
     <div>LoginPage</div>
   )
